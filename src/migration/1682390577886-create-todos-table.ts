201cbc2a5e9dbd2d908ea3ae2a035fe39cd29621
aliawada <awadinha99@gmail.com>
2023-04-25 00:59:12 -0300

    Nestjs back-end for To Do App

---
+++ src/migration/1682390577886-create-todos-table.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateTodosTable1682390577886 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`
+            create table todos(
+                id bigserial primary key,
+                name text,
+                completed boolean not null default false
+            )
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`drop table todos`);
+    }
+
+}
